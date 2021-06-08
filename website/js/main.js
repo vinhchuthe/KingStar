6197a6017f8bcc8d39ed437a66e48e3b5e4eacb6
vinhct <vinhchuthe@admicro.vn>
2021-06-08 17:17:32 +0700

    update code

---
+++ website/js/main.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+// Home
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
