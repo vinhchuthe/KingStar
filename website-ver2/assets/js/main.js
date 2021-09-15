cbd6d59df86cb3b33f8d252545c329407a51c624
vinhct <vinhchuthe@admicro.vn>
2021-09-15 14:31:53 +0700

    update

--- website-ver2/assets/js/main.js
+++ website-ver2/assets/js/main.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
     const swiper3 = new Swiper('#case-img', {
         // Optional parameters
         loop: true,
-        allowTouchMove: false,
+        speed: 1000,
+        longSwipesRatio: 0.01,
+        followFinger: false,
+        grabCursor: true,
+        // allowTouchMove: false,
         observer: true,
         observeParents: true,
 
@@ -25,6 +29,7 @@ $(document).ready(function () {
     });
 
     swiper4.controller.control = swiper3;
+    swiper3.controller.control = swiper4;
 
     const swiper1 = new Swiper('#logo1', {
         // Optional parameters
@@ -50,8 +55,6 @@ $(document).ready(function () {
             disableOnInteraction: false,
         },
     });
-
-
     const swiper5 = new Swiper('#brand-slide', {
         // Optional parameters
         loop: true,
@@ -68,7 +71,6 @@ $(document).ready(function () {
             },
         }
     });
-
     const swiper6 = new Swiper('#social-slide', {
         // Optional parameters
         loop: true,
@@ -90,14 +92,32 @@ $(document).ready(function () {
         }
     });
 
+
     $('#homeTab a').on('click', function (event) {
-        event.preventDefault()
-        $(this).tab('show')
+        event.preventDefault();
+        var name = $(this).attr('data-nav');
+        $(this).tab('show');
+
+        if (name === 'campaign') {
+            swiper5.destroy();
+            initSwiper5();
+        } else if (name === 'about') {
+            swiper1.destroy();
+            swiper2.destroy();
+            initSwiper1();
+            initSwiper2();
+        }
     });
 
     $('#contestTab a').on('click', function (event) {
-        event.preventDefault()
-        $(this).tab('show')
+        event.preventDefault();
+        var name = $(this).attr('data-nav');
+        $(this).tab('show');
+
+        if (name === 'themanh') {
+            swiper6.destroy();
+            initSwiper6();
+        }
     })
 
     $("#contact-link").click(function (e) {
@@ -108,3 +128,75 @@ $(document).ready(function () {
         return false;
     });
 });
+
+function initSwiper1() {
+    const swiper1 = new Swiper('#logo1', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 3,
+        // spaceBetween: 20,
+        observer: true,
+        observeParents: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+}
+
+function initSwiper2() {
+    const swiper2 = new Swiper('#logo2', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 3,
+        observer: true,
+        observeParents: true,
+        // spaceBetween: 20,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+}
+
+function initSwiper5() {
+    const swiper5 = new Swiper('#brand-slide', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 3,
+        observer: true,
+        observeParents: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            900: {
+                slidesPerView: 5,
+            },
+        }
+    });
+}
+
+function initSwiper6() {
+    const swiper6 = new Swiper('#social-slide', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 3,
+        observer: true,
+        observeParents: true,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 5,
+            },
+            1600: {
+                slidesPerView: 6,
+            },
+        }
+    });
+}
